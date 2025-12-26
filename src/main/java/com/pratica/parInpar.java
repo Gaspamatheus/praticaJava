455bf2f7830a6c704b2defd06cfb2567dfe4f478
Matheus Augusto Gasparetto <matheusgaspa98@gmail.com>
2025-12-26 08:47:21 -0300

    frist

---
+++ src/main/java/com/pratica/parInpar.java
@@ -0,0 +1,22 @@
+package com.pratica;
+
+import java.util.Scanner;
+
+public class parInpar {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Digite um número para verificar se é par ou impar");
+        int numero = scanner.nextInt();
+        System.out.println("O número digitado foi: " + numero);
+
+        int parImpar = numero % 2;
+
+        if(parImpar == 0){
+            System.out.println("Este número é par");
+        } else {
+            System.out.println("esse numero é impar");
+        }
+
+    }
+}
